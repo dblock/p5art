678e317df9b86e8d34735b1074ec428f82bbb0ec
dblock <dblock@dblock.org>
2021-06-06 13:18:14 -0400

    Danger/Dancer

--- build/build.js
+++ build/build.js
@@ -1,31 +1,19 @@
 var sanguine;
-var tr_x = 0;
-var direction = 1;
-var m = 10;
 var c;
-var iter = 0;
 function setup() {
-    c = createCanvas(windowWidth, windowHeight);
-    rectMode(CENTER).noFill().frameRate(30);
+    c = createCanvas(640, 640);
+    rectMode(CENTER).noFill().frameRate(1 / 5);
     sanguine = color('#850505');
+    textFont("Impact", windowHeight / 4);
+    textAlign(CENTER, CENTER);
+    background(255);
+    fill(sanguine);
 }
 function draw() {
-    background(255);
-    translate(width / 2, height / 1.5);
-    var x = width / 3;
-    var y = width / 3;
-    if (tr_x >= m)
-        direction = -1;
-    if (tr_x <= 1)
-        direction = 1;
-    tr_x = tr_x + direction * 0.25;
-    push();
-    fill(sanguine);
-    quad(-x - tr_x, -y, -x / 2 + 2 * tr_x, -y * 1.5, x * 1.1 - 2 * tr_x, -y * 0.5, -x * 0.2, y * 0.6 - tr_x);
-    pop();
-    if (iter < 10) {
-        iter += 1;
-    }
+    var s = (second() % 2 == 1) ? 'DANGER' : 'DANCER';
+    clear();
+    translate(width / 2, height / 2);
+    text(s, 0, 0);
 }
 function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
