36c61753c6a02d7c8fe41e871df98e69eb0e4c92
dblock <dblock@amazon.com>
2021-07-03 11:30:10 -0500

    AL ARM ED

--- build/build.js
+++ build/build.js
@@ -1,29 +1,30 @@
 var sanguine;
 var c;
-var angle = 0.0;
+var lines = 10;
 function setup() {
     c = createCanvas(640, 640);
-    rectMode(CENTER).noFill().frameRate(120);
-    sanguine = color('#000000');
-    textFont("Big Caslon", windowHeight / 3);
+    rectMode(CENTER).noFill().frameRate(1 / 3);
+    sanguine = color('#850505');
+    textFont("Impact", windowHeight / lines);
     textAlign(CENTER, CENTER);
     background(255);
     fill(sanguine);
-    angleMode(DEGREES);
+}
+function addSpace(s, count) {
+    var result = "";
+    for (var i = 0; i < count; i++) {
+        var index = random(0, s.length + 1);
+        result += s.substr(0, index) + " " + s.substr(index);
+    }
+    return result;
 }
 function draw() {
     clear();
-    translate(width / 2, height / 2);
-    rotate(angle);
-    if (angle === 180) {
-        frameRate(1 / 5);
-        angle = 0;
+    translate(width / 2 - width / lines / 3, height / lines / 2);
+    for (var i = 0; i < lines; i++) {
+        text(addSpace("ALARMED", 3), 0, 0);
+        translate(0, height / lines);
     }
-    else if (angle == 1) {
-        frameRate(120);
-    }
-    angle++;
-    text("1961", 0, 0);
 }
 function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
--- sketch/sketch.ts
+++ sketch/sketch.ts
@@ -1,30 +1,33 @@
 let sanguine: p5.Color;
 let c: p5.Renderer;
-let angle = 0.0;
+let lines = 10;
 
 function setup() {
   c = createCanvas(640, 640);
-  rectMode(CENTER).noFill().frameRate(120);
-  sanguine = color('#000000');
-  textFont("Big Caslon", windowHeight / 3);
+  rectMode(CENTER).noFill().frameRate(1/3);
+  sanguine = color('#850505');
+  textFont("Impact", windowHeight / lines);
   textAlign(CENTER, CENTER);
   background(255);
   fill(sanguine);
-  angleMode(DEGREES);
+}
+
+function addSpace(s: string, count: number) {
+  let result = "";
+  for (let i = 0; i < count; i++) {
+    let index = random(0, s.length + 1);
+    result += s.substr(0, index) + " " + s.substr(index);
+  } 
+  return result;
 }
 
 function draw() {
   clear();
-  translate(width / 2, height / 2);
-  rotate(angle);
-  if (angle === 180) {
-    frameRate(1 / 5);
-    angle = 0;
-  } else if (angle == 1) {
-    frameRate(120);
+  translate(width/2-width/lines/3, height/lines/2);
+  for (let i = 0; i < lines; i++) {
+    text(addSpace("ALARMED", 3), 0, 0);
+    translate(0, height / lines);
   }
-  angle++;
-  text("1961", 0, 0);
 }
 
 function windowResized() {
